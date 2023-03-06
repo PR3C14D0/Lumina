e864c9a4dee1e3abb3104d9090dd81d9dcabe614
PR3C14D0 <aarongarpre@gmail.com>
2023-03-06 22:15:03 +0100

    Started initializing D3D12

--- src/private/Engine/Core.cpp
+++ src/private/Engine/Core.cpp
@@ -16,6 +16,20 @@ void Core::Init() {
 		MessageBox(NULL, "No window found", "Error", MB_OK);
 		return;
 	}
+
+	UINT nFactoryCreateFlags = 0;
+
+#ifndef NDEBUG
+	nFactoryCreateFlags |= DXGI_CREATE_FACTORY_DEBUG;
+	{
+		ComPtr<ID3D12Debug1> dbg;
+		ThrowIfFailed(D3D12GetDebugInterface(IID_PPV_ARGS(dbg.GetAddressOf())));
+		dbg->EnableDebugLayer();
+		dbg->SetEnableGPUBasedValidation(TRUE);
+	}
+#endif // NDEBUG
+
+	ThrowIfFailed(CreateDXGIFactory2(nFactoryCreateFlags, IID_PPV_ARGS(this->factory.GetAddressOf())));
 }
 
 Core* Core::GetInstance() {
--- src/public/Engine/Core.h
+++ src/public/Engine/Core.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <Windows.h>
 #include <wrl.h>
+#include <d3d12.h>
 #include <dxgi1_4.h>
 #include "Engine/Util.h"
 
@@ -9,8 +10,12 @@ using namespace Microsoft::WRL;
 class Core {
 private:
 	static Core* instance;
-
 	HWND hwnd;
+
+	ComPtr<IDXGIFactory2> factory;
+	ComPtr<IDXGIAdapter> adapter;
+	ComPtr<IDXGISwapChain1> sc;
+	ComPtr<ID3D12Device> dev;
 public:
 	void SetHWND(HWND& hwnd);
 
