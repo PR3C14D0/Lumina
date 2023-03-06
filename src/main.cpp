a4ea164bffbdf5ebfebbc2a7d9a0bfb7179cff16
PR3C14D0 <aarongarpre@gmail.com>
2023-03-06 22:03:18 +0100

    Window creation

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,56 @@
 #include <Windows.h>
 
+bool g_quit = false;
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+	const char CLASS_NAME[] = "LuminaEngine";
+
+	WNDCLASS wc = { };
+	wc.lpszClassName = CLASS_NAME;
+	wc.hInstance = hInstance;
+	wc.lpfnWndProc = WndProc;
+
+	RegisterClass(&wc);
+
+	HWND hwnd = CreateWindowEx(
+		NULL,
+		CLASS_NAME,
+		"Lumina Engine",
+		WS_OVERLAPPEDWINDOW,
+
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+
+		NULL, NULL, hInstance, NULL
+	);
+
+	if (!hwnd) {
+		MessageBox(NULL, "An error occurred creating the window", "Error", MB_OK);
+		return TRUE;
+	}
+
+	ShowWindow(hwnd, nShowCmd);
+
+	MSG msg = { };
+
+	while (!g_quit) {
+		while (PeekMessage(&msg, NULL, NULL, NULL, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
 	return FALSE;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	switch (uMsg) {
+	case WM_DESTROY:
+		g_quit = true;
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
