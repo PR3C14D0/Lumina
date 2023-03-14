068d3b51ff92cbb3b50f15db63c7730569da3e9d
PR3C14D0 <aarongarpre@gmail.com>
2023-03-14 23:31:05 +0100

    Added minimal methods to our Transform class

--- src/private/Math/Transform.cpp
+++ src/private/Math/Transform.cpp
@@ -5,3 +5,23 @@ Transform::Transform() {
 	this->rotation = Vector3{0.f, 0.f, 0.f};
 	this->scale = Vector3{0.f, 0.f, 0.f};
 }
+
+void Transform::translate(Vector3 translation) {
+	this->location = this->location + translation;
+	return;
+}
+
+void Transform::translate(float x, float y, float z) {
+	this->translate(Vector3{ x, y, z });
+	return;
+}
+
+void Transform::rotate(Vector3 rotation) {
+	this->rotation = this->rotation + rotation;
+	return;
+}
+
+void Transform::rotate(float x, float y, float z) {
+	this->rotate(Vector3{ x, y, z });
+	return;
+}
--- src/public/Math/Transform.h
+++ src/public/Math/Transform.h
@@ -8,4 +8,10 @@ public:
 	Vector3 scale;
 
 	Transform();
+
+	void translate(Vector3 translation);
+	void translate(float x, float y, float z);
+
+	void rotate(Vector3 rotation);
+	void rotate(float x, float y, float z);
 };
