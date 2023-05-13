74a2a227494f8bcba2e1e976fe34acce8f5bc285
PR3C14D0 <aarongarpre@gmail.com>
2023-05-13 13:35:39 +0200

    Started with our light component

---
+++ src/private/Engine/GameObject/Component/Light.cpp
@@ -0,0 +1,5 @@
+#include "Engine/GameObject/Component/Light.h"
+
+Light::Light() : Component::Component() {
+	
+}
---
+++ src/public/Engine/GameObject/Component/Light.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "Engine/GameObject/Component/Component.h"
+
+class Light : public Component {
+public:
+	Light();
+};
